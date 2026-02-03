cd6200524097c13d531a835cdbe3aca72f4d5a25
Jitendra Kumar <99542823+jitendra143rn@users.noreply.github.com>
2026-02-03 23:10:33 +0530

    Add MaxHeap implementation with insert function
    Implement a MaxHeap structure with insert functionality.

---
+++ Heap/Insert.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+#define MAX_SIZE 100
+
+typedef struct
+{
+    int arr[MAX_SIZE];
+    int size;
+
+} MaxHeap;
+
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void Insert(MaxHeap *h, int val)
+{
+    int i = h->size;
+    h->arr[i] = val;
+    h->size++;
+
+    while(i !=0 && h->arr[(i-1)/2] < h->arr[i])
+    {
+        swap(&h->arr[i], &h->arr[(i-1)/2]);
+        i = (i -1)/2;
+    }
+
+}
+int main(void)
+{
+    MaxHeap h;
+    h.size = 0; 
+
+    Insert(&h, 50);
+    Insert(&h, 30);
+    Insert(&h, 40);
+    Insert(&h, 60);
+
+    for(int i = 0; i < h.size; i++)
+    {
+        printf("%d ", h.arr[i]);
+    }
+
+    return 0; 
+
+}
